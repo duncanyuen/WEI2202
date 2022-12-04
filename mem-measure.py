5a92ebc78518ccba65ac2875c443ff32b5f2782e
Duncan Yuen <duncan@duncan-debian7.duncanyuen>
2022-12-05 00:05:23 +0800

    Added benchmarking logs and custom rules

--- mem-measure.py
+++ mem-measure.py
@@ -1,7 +1,8 @@
 import time
-import subprocess
 import argparse
 import sys
+import psutil
+import os
 
 def get_used_mem() -> int:
     mem_file = "/proc/meminfo"
@@ -12,18 +13,23 @@ def get_used_mem() -> int:
     used_mem = int(total_mem.split()[1]) - int(avail_mem.split()[1])
     return used_mem
 
-def get_cpu_usage() -> float:
-    output = subprocess.check_output("mpstat -u")
-    print(output)
+def get_cpu_usage_1m() -> float:
+    load1m = (psutil.getloadavg()[0]/os.cpu_count()) * 100
+    return load1m
 
 if __name__ == "__main__":
-    get_cpu_usage()
-    sys.exit(0)
     parser = argparse.ArgumentParser(
                     prog = 'mem-measure.py')
     parser.add_argument("-m", "--mem_filename")
+    parser.add_argument("-c", "--cpu_filename")
     args = parser.parse_args()
+    counter = 0
     while True:
         with open(args.mem_filename, "a") as f:
             f.write(str(get_used_mem())+"\n")
         time.sleep(5)
+        counter = counter + 1
+        if counter % 12 == 0:
+            with open(args.cpu_filename, "a") as f:
+                f.write(str(get_cpu_usage_1m())+"\n")
+            counter = 0
